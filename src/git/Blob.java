50dd56f0dcef0794aed56a59f259301a7308da9b
Audrey Yang <audreytyang@gmail.com>
2022-09-01 10:34:03 -0700

    Create and write Blob class

---
+++ src/git/Blob.java
@@ -0,0 +1,38 @@
+package git;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Blob {
+	
+	public Blob (String filePath) throws IOException, NoSuchAlgorithmException {
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        DigestInputStream digestInputStream = new DigestInputStream(fileInputStream, digest);
+        byte[] bytes = new byte[1024];
+        // read all file content
+        while (digestInputStream.read(bytes) > 0);
+
+//        digest = digestInputStream.getMessageDigest();
+        byte[] resultByteArry = digest.digest();
+        System.out.println (bytesToHexString(resultByteArry));
+    }
+	
+	public static String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            int value = b & 0xFF;
+            if (value < 16) {
+                // if value less than 16, then it's hex String will be only
+                // one character, so we need to append a character of '0'
+                sb.append("0");
+            }
+            sb.append(Integer.toHexString(value).toUpperCase());
+        }
+        return sb.toString();
+    }
+}
+
---
+++ src/testers/BlobTester.java
@@ -0,0 +1,14 @@
+package testers;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import git.Blob;
+
+public class BlobTester {
+	public static void main (String [] args) throws IOException, NoSuchAlgorithmException {
+		Blob blob = new Blob ("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/someFile.rtf");
+		
+	}
+	
+
+}
