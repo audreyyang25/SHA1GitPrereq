ad7b9944e92735db1f9c15041ca9b55bd5b44039
Audrey Yang <audreytyang@gmail.com>
2022-09-01 16:28:48 -0700

    Edited output of Blob constructor and method that reads file content

--- src/git/Blob.java
+++ src/git/Blob.java
@@ -1,12 +1,22 @@
 package git;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Blob {
+	private static String content;
 	
 	public Blob (String filePath) throws IOException, NoSuchAlgorithmException {
         FileInputStream fileInputStream = new FileInputStream(filePath);
@@ -18,9 +28,42 @@ public class Blob {
 
 //        digest = digestInputStream.getMessageDigest();
         byte[] resultByteArry = digest.digest();
-        System.out.println (bytesToHexString(resultByteArry));
+        String hash = bytesToHexString(resultByteArry);
+        
+        content = Blob.content(filePath);
+        
+        System.out.println ("Reading contents of " + filePath + ": " + content);
+        
+        
+        System.out.println ("Creating new blob " + hash + " from content: " + content);
+////        Blob.createsNewFile(hash);
     }
 	
+	public static String content (String filepath) throws IOException {
+		File file = new File (filepath);
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		StringBuilder sb = new StringBuilder(); 
+		String line = br.readLine(); 
+		while (line != null) { 
+			sb.append(line).append("\n"); 
+			line = br.readLine(); 
+			} 
+		String fileAsString = sb.toString();
+		return fileAsString;
+	}
+	
+	public static void createsNewFile (String hash) throws FileNotFoundException, UnsupportedEncodingException {
+		File file = new File ("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test");
+		PrintWriter writer = new PrintWriter(file, "UTF-8");
+		writer.println (content);
+		writer.close();
+	}
+	
+	private static File File(String hash) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	public static String bytesToHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (byte b : bytes) {
--- src/testers/BlobTester.java
+++ src/testers/BlobTester.java
@@ -6,8 +6,9 @@ import git.Blob;
 
 public class BlobTester {
 	public static void main (String [] args) throws IOException, NoSuchAlgorithmException {
-		Blob blob = new Blob ("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/someFile.rtf");
-		
+		Blob blob = new Blob ("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/bbb.txt");
+		System.out.println (Blob.content("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/bbb.txt"));
+		Blob.createsNewFile("aaa");
 	}
 	
 
