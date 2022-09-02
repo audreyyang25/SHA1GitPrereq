bd53cec54b13a607ce60f5e80dce56e4fcd7436a
Audrey Yang <audreytyang@gmail.com>
2022-09-02 07:55:26 -0700

    Edited zip method to only create zip files and used relative filepaths

--- src/git/Blob.java
+++ src/git/Blob.java
@@ -24,33 +24,37 @@ public class Blob {
 	private static String hash;
 	private static String zipContent;
 	
+	//should it be generalized to work on all computers, or just mine?
 	public Blob (String filePath) throws IOException, NoSuchAlgorithmException {
-        FileInputStream fileInputStream = new FileInputStream(filePath);
-        MessageDigest digest = MessageDigest.getInstance("SHA-1");
-        DigestInputStream digestInputStream = new DigestInputStream(fileInputStream, digest);
-        byte[] bytes = new byte[1024];
-        // read all file content
-        while (digestInputStream.read(bytes) > 0);
+		Blob.generateSHA1Hash(filePath);
+		content = Blob.content(filePath);
+		String hashFile = Blob.createsNewFile();
+		String zipFilePath = Blob.zipFile(hashFile);
+		zipContent = Blob.content(zipFilePath);
+		System.out.println ("Reading contents of " + filePath + ": " + content);
 
-//        digest = digestInputStream.getMessageDigest();
-        byte[] resultByteArry = digest.digest();
-        hash = bytesToHexString(resultByteArry);
-        content = Blob.content(filePath);
-        Blob.createsNewFile();
-        
-        String zipFilePath = Blob.zipFile("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/objects/" +hash);
-        zipContent = Blob.content(zipFilePath);
-        System.out.println ("Reading contents of " + filePath + ": " + content);
-        
-        
-        System.out.println ("Creating new blob " + hash + " from content: " + zipContent);
-        
-    }
-	
-	public String getSHA1Hash () {
+
+		System.out.println ("Creating new blob " + hash + " from content: " + zipContent);
+		//should this only display the first few letters of the hash?
+
+	}
+
+	//generates a Hash String for given filepath
+	public static String generateSHA1Hash (String filePath) throws IOException, NoSuchAlgorithmException {
+		FileInputStream fileInputStream = new FileInputStream(filePath);
+		MessageDigest digest = MessageDigest.getInstance("SHA-1");
+		DigestInputStream digestInputStream = new DigestInputStream(fileInputStream, digest);
+		byte[] bytes = new byte[1024];
+		// read all file content
+		while (digestInputStream.read(bytes) > 0);
+
+		//	        digest = digestInputStream.getMessageDigest();
+		byte[] resultByteArry = digest.digest();
+		hash = bytesToHexString(resultByteArry);
 		return hash;
 	}
-	
+
+	// reads contents of the file
 	public static String content (String filepath) throws IOException {
 		File file = new File (filepath);
 		BufferedReader br = new BufferedReader(new FileReader(file)); 
@@ -59,48 +63,63 @@ public class Blob {
 		while (line != null) { 
 			sb.append(line).append("\n"); 
 			line = br.readLine(); 
-			} 
+		} 
 		String fileAsString = sb.toString();
 		return fileAsString;
-	}
-	
-	public static void createsNewFile () throws IOException {
-		FileWriter writer = new FileWriter("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/objects/" +hash);
+	} 
+
+	// creates new file in objects folder labeled by hash with same content as original
+	public static String createsNewFile () throws IOException {
+//		FileWriter writer = new FileWriter("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/objects/" +hash);
+		File f = new File ("./test/objects/" + hash);
+		String path = f.getAbsolutePath();
+		FileWriter writer = new FileWriter(path);
+		
 		writer.write (content);
 		writer.close();
+		return path;
 	}
 
-	
+	// converts file to zipped format
+	//when you unzip, should it open in the objects folder too?
+	//should the zip file end in ".zip"?
 	public static String zipFile (String sourceFile) throws IOException {
-        FileOutputStream fos = new FileOutputStream(sourceFile);
-        ZipOutputStream zipOut = new ZipOutputStream(fos);
-        File fileToZip = new File(sourceFile);
-        FileInputStream fis = new FileInputStream(fileToZip);
-        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-        zipOut.putNextEntry(zipEntry);
-        byte[] bytes = new byte[1024];
-        int length;
-        while((length = fis.read(bytes)) >= 0) {
-            zipOut.write(bytes, 0, length);
-        }
-        zipOut.close();
-        fis.close();
-        fos.close();
-        return sourceFile;
-}
-	
+		File f = new File (sourceFile);
+//		String sourcePath = f.getAbsolutePath();
+//		System.out.println(sourcePath);
+//		f = new File (sourcePath);
+		String zipFile = sourceFile + ".zip";
+		FileOutputStream fos = new FileOutputStream(zipFile);
+		ZipOutputStream zipOut = new ZipOutputStream(fos);
+		File fileToZip = new File(sourceFile);
+		FileInputStream fis = new FileInputStream(fileToZip);
+		ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+		zipOut.putNextEntry(zipEntry);
+		byte[] bytes = new byte[1024];
+		int length;
+		while((length = fis.read(bytes)) >= 0) {
+			zipOut.write(bytes, 0, length);
+		}
+		zipOut.close();
+		fis.close();
+		fos.close();
+		f.delete();
+		return zipFile;
+	}
+
+	// some method that helps with generating hash
 	public static String bytesToHexString(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            int value = b & 0xFF;
-            if (value < 16) {
-                // if value less than 16, then it's hex String will be only
-                // one character, so we need to append a character of '0'
-                sb.append("0");
-            }
-            sb.append(Integer.toHexString(value).toUpperCase());
-        }
-        return sb.toString();
-    }
+		StringBuilder sb = new StringBuilder();
+		for (byte b : bytes) {
+			int value = b & 0xFF;
+			if (value < 16) {
+				// if value less than 16, then it's hex String will be only
+				// one character, so we need to append a character of '0'
+				sb.append("0");
+			}
+			sb.append(Integer.toHexString(value).toUpperCase());
+		}
+		return sb.toString();
+	}
 }
 
--- src/testers/BlobTester.java
+++ src/testers/BlobTester.java
@@ -1,4 +1,5 @@
 package testers;
+import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
@@ -6,11 +7,14 @@ import git.Blob;
 
 public class BlobTester {
 	public static void main (String [] args) throws IOException, NoSuchAlgorithmException {
-		Blob blob = new Blob ("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/bbb.txt");
-		Blob blob2 = new Blob ("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/someFile.txt");
+		Blob blob = new Blob ("./test/bbb.txt");
+		Blob blob2 = new Blob ("./test/someFile.txt");
+		Blob blob3 = new Blob ("./test/anotherFile");
 //		System.out.println (Blob.content("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/bbb.txt"));
 //		System.out.println (blob.getSHA1Hash());
 //		Blob.zipFile("/Users/audreyyang/eclipse-workspace/SHA1GitPrereq/test/bbb.txt");
+//		File f = new File ("test/bbb.txt");
+//		System.out.println (f.getAbsolutePath());
 	}
 	
 
